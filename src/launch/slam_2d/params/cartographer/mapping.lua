a900dafdddcab88e69b56ab43258c35dcc2ebe89
Sam <huangyinghuan99@gmail.com>
2024-01-02 14:50:04 +0800

    在odom 设置目标点可导航

--- src/launch/slam_2d/params/cartographer/mapping.lua
+++ src/launch/slam_2d/params/cartographer/mapping.lua
@@ -22,17 +22,17 @@ options = {
   tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
-  provide_odom_frame = false,
+  provide_odom_frame = true,
   publish_frame_projected_to_2d = true,
   use_odometry = true,
   use_nav_sat = false,
-  use_landmarks = true,
+  use_landmarks = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.5,
-  submap_publish_period_sec = 0.5,
+  submap_publish_period_sec = 0.1,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
@@ -43,11 +43,11 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
+-- TRAJECTORY_BUILDER_2D.use_landmarks = true
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.1
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 30
-
+-- TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+-- TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.1
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 30
 
 return options
